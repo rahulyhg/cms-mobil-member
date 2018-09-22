f1fabfd171321f5a292ed1e8808e0e3da2842c9f
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-09-22 17:20:21 +0700

    landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,169 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+@section('slider')
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="{{ asset('inset/homeslide-1.jpg') }}">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="{{ asset('inset/homeslide-1.jpg') }}">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="{{ asset('inset/homeslide-1.jpg') }}">
+        </div>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-6 nav-item-menu" style="-webkit-box-shadow:0 2px 4px rgba(0,0,0,.04);box-shadow:0 2px 4px rgba(0,0,0,.04);">
+        <form>
+            <div class="row" style="padding: 10px;">
+                <div class="col-md-4">
+                    <select class="form-control" style="width: 100%;">
+                        <option>1</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <select class="form-control" style="width: 100%;">
+                        <option>1</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <button class="btn btn-primary" style="width: 100%;">Cari</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-12" style="background-color: #ebe9ea;padding: 50px;">
+        <div class="form-group">
+            <center><h1>Mobil Pilihan</h1></center>
+        </div>
+        <div class="form-group">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <div class="card card-item border-danger" style="background-color: transparent;">
+                        <div class="card-body">
+                            <img src="{{ asset('inset/AllNewFortuner-white.png') }}" class="img-fluid">                        
+                            <h5 class="text-center"><b>All New Fortuner</b></h5>
+                            <h4 class="text-center"><b>RP100000</b></h4>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card-item border-danger" style="background-color: transparent;">
+                        <div class="card-body">
+                            <img src="{{ asset('inset/AllNewFortuner-white.png') }}" class="img-fluid">
+                            <h5 class="text-center"><b>All New Fortuner</b></h5>
+                            <h4 class="text-center"><b>RP100000</b></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card-item border-danger" style="background-color: transparent;">
+                        <div class="card-body">
+                            <img src="{{ asset('inset/AllNewFortuner-white.png') }}" class="img-fluid">
+                            <h5 class="text-center"><b>All New Fortuner</b></h5>
+                            <h4 class="text-center"><b>RP100000</b></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card-item border-danger" style="background-color: transparent;">
+                        <div class="card-body">
+                            <img src="{{ asset('inset/AllNewFortuner-white.png') }}" class="img-fluid">
+                            <h5 class="text-center"><b>All New Fortuner</b></h5>
+                            <h4 class="text-center"><b>RP100000</b></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card-item border-danger" style="background-color: transparent;">
+                        <div class="card-body">
+                            <img src="{{ asset('inset/AllNewFortuner-white.png') }}" class="img-fluid">
+                            <h5 class="text-center"><b>All New Fortuner</b></h5>
+                            <h4 class="text-center"><b>RP100000</b></h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="form-group">
+            <div class="row justify-content-center">
+                <button class="btn btn-primary">Lihat Semua</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-12" style="background-color: white;padding: 50px;">
+        <div class="form-group">
+            <center><h1>Testimoni Oleh Customer</h1></center>
+        </div>
+        <div class="form-group">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card card-item border-danger" style="background-color: transparent;">
+                        <div class="card-body">
+                            <img src="{{ asset('inset/AllNewFortuner-white.png') }}" class="img-fluid">
+                            <h5><b>David</b></h5>
+                            <h5><p><b>"Terimakasih tim Mobilngetop yang sangat ramah sudah membantu menemukan mobil sesuai budget saya dan juga selalu available. Recommend Banget!!!!"</b></p></h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card-item border-danger" style="background-color: transparent;">
+                        <div class="card-body">
+                            <img src="{{ asset('inset/AllNewFortuner-white.png') }}" class="img-fluid">
+                            <h5><b>David</b></h5>
+                            <h5><p><b>"Terimakasih tim Mobilngetop yang sangat ramah sudah membantu menemukan mobil sesuai budget saya dan juga selalu available. Recommend Banget!!!!"</b></p></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-12" style="background-color: #ebe9ea;padding: 50px;">
+        <div class="form-group">
+            <center><h1>Berita Otomotif</h1></center>
+        </div>
+        <div class="form-group">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <div class="card mb-3">
+                        <img class="card-img-top" src="{{ asset('inset/AllNewFortuner-white.png') }}">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Mobil Langkanya Hilang Pria ini Rela Buang Uang Rp 4,1 Milyar!</b></h5>
+                            <p class="card-text">Bagi siapapun yang dapat menemukan keberadaan Mercedez-Benz 300SL Gullwing '1955 ini akan mendapatkan hadiah berupa uang tunai sebesar USD284 ribu atau setara dengan 4,1 Milyaran, Uang tersebut  dijanjikan oleh pemilik yang kehilangan sedan langka tersebut.</p>
+                            <p class="card-text"><small style="color: #006db8;">ditulis oleh Nia pukul 18:27</small></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card mb-3">
+                        <img class="card-img-top" src="{{ asset('inset/AllNewFortuner-white.png') }}">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Mobil Langkanya Hilang Pria ini Rela Buang Uang Rp 4,1 Milyar!</b></h5>
+                            <p class="card-text">Bagi siapapun yang dapat menemukan keberadaan Mercedez-Benz 300SL Gullwing '1955 ini akan mendapatkan hadiah berupa uang tunai sebesar USD284 ribu atau setara dengan 4,1 Milyaran, Uang tersebut  dijanjikan oleh pemilik yang kehilangan sedan langka tersebut.</p>
+                            <p class="card-text"><small style="color: #006db8;">ditulis oleh Nia pukul 18:27</small></p>
+                        </div>
+                    </div>              
+                </div>
+                <div class="col">
+                    <div class="card mb-3">
+                        <img class="card-img-top" src="{{ asset('inset/AllNewFortuner-white.png') }}">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Mobil Langkanya Hilang Pria ini Rela Buang Uang Rp 4,1 Milyar!</b></h5>
+                            <p class="card-text">Bagi siapapun yang dapat menemukan keberadaan Mercedez-Benz 300SL Gullwing '1955 ini akan mendapatkan hadiah berupa uang tunai sebesar USD284 ribu atau setara dengan 4,1 Milyaran, Uang tersebut  dijanjikan oleh pemilik yang kehilangan sedan langka tersebut.</p>
+                            <p class="card-text"><small style="color: #006db8;">ditulis oleh Nia pukul 18:27</small></p>
+                        </div>
+                    </div>              
+                </div>
+            </div>
+        </div>        
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,13 +19,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{ asset('inset/logo.png') }}" class="img-fluid" width="100">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,46 +35,69 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link d-md-none d-lg-none d-xl-none" href="#">Mobil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-md-none d-lg-none d-xl-none" href="#">Promo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-md-none d-lg-none d-xl-none" href="#">Artikel</a>
+                        </li>
                     </ul>
+
+                    <div class="row justify-content-center">
+                        <ul class="nav nav-tabs mx-auto" style="margin-bottom: -20px;">
+                            <li class="nav-item">
+                                <a class="nav-link nav-item-menu d-none d-sm-none d-md-block d-lg-block d-xl-block" href="#">Mobil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link nav-item-menu d-none d-sm-none d-md-block d-lg-block d-xl-block" href="#">Promo</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link nav-item-menu d-none d-sm-none d-md-block d-lg-block d-xl-block" href="#">Artikel</a>
+                            </li>
+                        </ul>
+                    </div>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
+                        <li class="nav-item">
+                            <div class="col"></div>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+                        </li>                        
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        </nav>    
+        @yield('slider')        
+        @yield('content')            
+        <footer class="footer">
+            <div class="container">
+                <span class="text-muted">Place sticky footer content here.</span>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
