6532c899c15510340e10c146f617beb737d7a928
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-10-07 21:01:07 +0700

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,33 +5,39 @@
     <form class="login-form" method="POST" action="{{ route('login') }}">
         @csrf
         <div class="row justify-content-center">
-            <div class="col-sm-6">
-                <center>
-                    <img src="{{ asset('inset/login.png') }}" class="img-fluid">
-                </center>
+            <div class="col-sm-6 text-center">
+                <img src="{{ asset('inset/login.png') }}" class="img-fluid">
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                 <div class="form-group">                    
-                    <i class="fa fa-envelope-o fa-fw">Email</i>
-                    <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" style="color: black;background-color: transparent;border:0px;"><i class="fa fa-envelope fa-fw fa-2x"></i></span>
+                        </div>
+                        <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="email" required autofocus>
 
-                    @if ($errors->has('email'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                    @endif
+                        @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                        @endif
+                    </div>
                 </div>
                 <div class="form-group">                    
-                    <i class="fa fa-lock fa-fw">Password</i>
-                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" style="color: black;background-color: transparent;border:0px;"><i class="fa fa-lock fa-fw fa-2x"></i></span>
+                        </div>
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="password">
 
-                    @if ($errors->has('password'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                    @endif
+                        @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
