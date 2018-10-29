cce8e44422ef9077f30d0213531c00768ee480d9
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-10-29 10:10:41 +0700

    promo

--- resources/views/member/promo.blade.php
+++ resources/views/member/promo.blade.php
@@ -4,19 +4,21 @@
 <div class="container">
 	<div class="row justify-content-center" style="padding: 10px;">
 		<div class="col-sm-12" style="padding-bottom: 10px;">
-			@foreach($promos as $promo)
-			<div class="col-sm-4">
-				<div class="card card-item border-info" style="background-color: transparent;">
-					<div style="padding: 10px;">
-						<div class="card-body" style="padding: 0px;flex: 0;">
-							<center>
-								<img src="{{ url('https://admin.mobilngetop.com/'.$promo->picture) }}" class="img-fluid">
-							</center>
+			<div class="row justify-content-center">
+				@foreach($promos as $promo)
+				<div class="col-sm-4">
+					<div class="card card-item border-info" style="background-color: transparent;">
+						<div style="padding: 10px;">
+							<div class="card-body" style="padding: 0px;flex: 0;">
+								<center>
+									<img src="{{ url('https://admin.mobilngetop.com/'.$promo->picture) }}" class="img-fluid">
+								</center>
+							</div>
 						</div>
 					</div>
 				</div>
+				@endforeach
 			</div>
-			@endforeach
 		</div>
 		<div class="col-sm-12" style="padding-bottom: 10px;">
 			<h2>Syarat & Ketentuan:</h2>
