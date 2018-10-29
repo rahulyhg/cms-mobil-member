28c70335943621319802824fc11274c2e83c9479
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-10-29 10:06:17 +0700

    promo

--- resources/views/member/promo.blade.php
+++ resources/views/member/promo.blade.php
@@ -5,7 +5,15 @@
 	<div class="row justify-content-center" style="padding: 10px;">
 		<div class="col-sm-12" style="padding-bottom: 10px;">
 			@foreach($promos as $promo)
-			{{ $promo->picture }}<br>
+			<div class="col-sm-4">
+				<div class="card card-item border-info" style="background-color: transparent;">
+					<div style="padding: 10px;">
+						<div class="card-body" style="padding: 0px;flex: 0;">
+							<img src="{{ asset($promo->picture) }}" class="img-fluid">
+						</div>
+					</div>
+				</div>
+			</div>
 			@endforeach
 		</div>
 		<div class="col-sm-12" style="padding-bottom: 10px;">
