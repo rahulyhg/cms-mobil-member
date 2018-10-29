c710505e69be85b92b7a5e5b0e3fc636360d16d8
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-10-29 09:59:41 +0700

    promo

--- resources/views/member/promo.blade.php
+++ resources/views/member/promo.blade.php
@@ -3,15 +3,19 @@
 @section('content')
 <div class="container">
 	<div class="row justify-content-center" style="padding: 10px;">
-		<div class="col-sm-8" style="padding-bottom: 10px;">
-			<h2 class="text-danger">FAQ</h2>
+		<div class="col-sm-12" style="padding-bottom: 10px;">
+			@foreach($promos as $promo)
+			{{ $promo->picture }}<br>
+			@endforeach
 		</div>
-		@foreach($faqs as $faq)
-		<div class="col-sm-8" style="padding-bottom: 10px;">
-			<h3 style="color: #006db8;">{{ $faq->name }}</h3>
-			<p style="word-break: all;"><b>{{ $faq->content }}</b></p>
+		<div class="col-sm-12" style="padding-bottom: 10px;">
+			<h2>Syarat & Ketentuan:</h2>
 		</div>
-		@endforeach        
+		<div class="col-sm-12" style="padding-bottom: 10px;">
+			@foreach($about_promos as $about_promo)
+			<p style="word-break: all;"><b>- {{ $about_promo->content }}</b></p>
+			@endforeach        
+		</div>
 	</div>
 </div>
 @endsection
